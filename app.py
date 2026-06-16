5f1cbd650f3b89086c6740b4fa8be7ab38028d0c
mj16-hc <mario.huaracachoque@gmail.com>
2026-06-15 20:31:10 -0500

    Adding If

--- app.py
+++ app.py
@@ -15,4 +15,15 @@ st.write("Elaborado por MJHC")
 ##Carga de Archivo
 archivo = st.file_uploader("Cargue el archivo excel o csv")
 
-
+##Validación de archivo
+if archivo is not None :
+  if archivo.name.endswith(".csv"):
+    data = pd.read_csv(archivo)
+    st.write(data)
+  elif archivo.name.endswith(".xlsx"):
+    data = pd.read_excel(archivo)
+    st.write(data)
+  else:
+    st.write("Formato no válido")
+else :
+  st.write("Por favor cargue su archivo")
